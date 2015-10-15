cace4caf7536cd499f6d29e0dbcc4b9d9aea0b2a
NikoNeugebauer <info@webcaravela.com>
2015-10-16 00:27:35 +0100

    Update dictionaries.sql

--- SQL-2014/dictionaries.sql
+++ SQL-2014/dictionaries.sql
@@ -1,7 +1,7 @@
 /*
 	Columnstore Indexes Scripts Library for SQL Server 2014: 
 	Dictionaries Analysis - Shows detailed information about the Columnstore Dictionaries
-	Version: Release 1, September 2015
+	Version: 1.0.1, October 2015
 
 	Copyright 2015 Niko Neugebauer, OH22 IS (http://www.nikoport.com/columnstore/), (http://www.oh22.is/)
 
