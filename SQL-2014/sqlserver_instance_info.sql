404be02aea52cac509c8a4a4dbdd0c82939b0193
NikoNeugebauer <info@webcaravela.com>
2015-10-16 00:29:16 +0100

    Update sqlserver_instance_info.sql

--- SQL-2014/sqlserver_instance_info.sql
+++ SQL-2014/sqlserver_instance_info.sql
@@ -1,7 +1,7 @@
 /*
 	Columnstore Indexes Scripts Library for SQL Server 2014: 
 	SQL Server Instance Information - Provides with the list of the known SQL Server versions that have bugfixes or improvements over your current version + lists currently enabled trace flags on the instance & session
-	Version: Release 1, September 2015
+	Version: 1.0.1, October 2015
 
 	Copyright 2015 Niko Neugebauer, OH22 IS (http://www.nikoport.com/columnstore/), (http://www.oh22.is/)
 
@@ -9,7 +9,7 @@
 	you may not use this file except in compliance with the License.
 	You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
