2de0787201364d22235ce9f70085477b4a730787
Niko Neugebauer <info@webcaravela.com>
2015-11-04 10:20:43 -0800

    Create cstore_GetSQLInfo.sql

---
+++ SQL-2016/StoredProcs/cstore_GetSQLInfo.sql
@@ -0,0 +1,189 @@
+/*
+	Columnstore Indexes Scripts Library for SQL Server 2016: 
+	SQL Server Instance Information - Provides with the list of the known SQL Server versions that have bugfixes or improvements over your current version + lists currently enabled trace flags on the instance & session
+	Version: 1.0.2, November 2015
+
+	Copyright 2015 Niko Neugebauer, OH22 IS (http://www.nikoport.com/columnstore/), (http://www.oh22.is/)
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/*
+	Known Issues & Limitations: 
+		- Custom non-standard (non-CU & non-SP) versions are not targeted yet
+		- Duplicate Fixes & Improvements (CU12 for SP1 & CU2 for SP2, for example) are not eliminated from the list yet
+*/
+
+--------------------------------------------------------------------------------------------------------------------
+declare @SQLServerVersion nvarchar(128) = cast(SERVERPROPERTY('ProductVersion') as NVARCHAR(128)), 
+		@SQLServerEdition nvarchar(128) = cast(SERVERPROPERTY('Edition') as NVARCHAR(128)),
+		@SQLServerBuild smallint = NULL;
+declare @errorMessage nvarchar(512);
+
+-- Ensure that we are running SQL Server 2016
+if substring(@SQLServerVersion,1,CHARINDEX('.',@SQLServerVersion)-1) <> N'13'
+begin
+	set @errorMessage = (N'You are not running a SQL Server 2016. Your SQL Server version is ' + @SQLServerVersion);
+	Throw 51000, @errorMessage, 1;
+end
+
+if SERVERPROPERTY('EngineEdition') <> 3 
+begin
+	set @errorMessage = (N'Your SQL Server 2016 Edition is not an Enterprise or a Developer Edition: Your are running a ' + @SQLServerEdition);
+	Throw 51000, @errorMessage, 1;
+end
+
+--------------------------------------------------------------------------------------------------------------------
+if EXISTS (select * from sys.objects where type = 'p' and name = 'cstore_GetSQLInfo' and schema_id = SCHEMA_ID('dbo') )
+	Drop Procedure dbo.cstore_GetSQLInfo;
+GO
+
+create procedure dbo.cstore_GetSQLInfo(
+-- Params --
+	@showUnrecognizedTraceFlags bit = 1,		-- Enables showing active trace flags, even if they are not columnstore indexes related
+	@identifyCurrentVersion bit = 1,			-- Enables identification of the currently used SQL Server Instance version
+	@showNewerVersions bit = 0					-- Enables showing the SQL Server versions that are posterior the current version-- end of --
+) as 
+begin
+	declare @SQLServerVersion nvarchar(128) = cast(SERVERPROPERTY('ProductVersion') as NVARCHAR(128)), 
+			@SQLServerEdition nvarchar(128) = cast(SERVERPROPERTY('Edition') as NVARCHAR(128));
+
+	declare @SQLServerBuild smallint  = substring(@SQLServerVersion,CHARINDEX('.',@SQLServerVersion,5)+1,CHARINDEX('.',@SQLServerVersion,8)-CHARINDEX('.',@SQLServerVersion,5)-1);
+
+	if OBJECT_ID('tempdb..#SQLColumnstoreImprovements', 'U') IS NOT NULL
+		drop table #SQLColumnstoreImprovements;
+	if OBJECT_ID('tempdb..#SQLBranches', 'U') IS NOT NULL
+		drop table #SQLBranches;
+	if OBJECT_ID('tempdb..#SQLVersions', 'U') IS NOT NULL
+		drop table #SQLVersions;
+
+	-- Returns tables suggested for using Columnstore Indexes for the DataWarehouse environments
+	create table #SQLColumnstoreImprovements(
+		BuildVersion smallint not null,
+		SQLBranch char(3) not null,
+		Description nvarchar(500) not null,
+		URL nvarchar(1000)
+	);
+
+	create table #SQLBranches(
+		SQLBranch char(3) not null Primary Key,
+		MinVersion smallint not null );
+
+	create table #SQLVersions(
+		SQLBranch char(3) not null,
+		SQLVersion smallint not null Primary Key,
+		SQLVersionDescription nvarchar(100) );
+
+	insert into #SQLBranches (SQLBranch, MinVersion)
+		values ('CTP', 200 );
+
+	insert #SQLVersions( SQLBranch, SQLVersion, SQLVersionDescription )
+		values 
+		( 'CTP', 200, 'CTP 2 for SQL Server 2016' ),
+		( 'CTP', 300, 'CTP 2.1 for SQL Server 2016' ),
+		( 'CTP', 400, 'CTP 2.2 for SQL Server 2016' ),
+		( 'CTP', 500, 'CTP 2.3 for SQL Server 2016' ),
+		( 'CTP', 600, 'CTP 2.4 for SQL Server 2016' ),
+		( 'CTP', 700, 'CTP 3 for SQL Server 2016' );
+
+
+	if @identifyCurrentVersion = 1
+	begin
+		if OBJECT_ID('tempdb..#TempVersionResults') IS NOT NULL
+			drop table #TempVersionResults;
+
+		create table #TempVersionResults(
+			MessageText nvarchar(512) NOT NULL,		
+			SQLVersionDescription nvarchar(200) NOT NULL,
+			SQLBranch char(3) not null,
+			SQLVersion smallint NULL );
+
+		if( exists (select 1
+						from #SQLVersions
+						where SQLVersion = cast(@SQLServerBuild as int) ) )
+			select 'You are Running:' as MessageText, SQLVersionDescription, SQLBranch, SQLVersion as BuildVersion
+				from #SQLVersions
+				where SQLVersion = cast(@SQLServerBuild as int);
+		else
+			select 'You are Running a Non RTM/SP/CU standard version:' as MessageText, '-' as SQLVersionDescription, 
+				ServerProperty('ProductLevel') as SQLBranch, @SQLServerBuild as SQLVersion;
+
+		-- Select information about all newer SQL Server versions that are known
+		if @showNewerVersions = 1
+		begin 
+			insert into #TempVersionResults
+				select 'Available Newer Versions:' as MessageText, '' as SQLVersionDescription, 
+					'' as SQLBranch, NULL as BuildVersion
+				UNION ALL
+				select '' as MessageText, SQLVersionDescription as SQLVersionDescription, 
+						SQLBranch as SQLVersionDescription, SQLVersion as BuildVersion
+						from #SQLVersions
+						where  @SQLServerBuild <  SQLVersion;
+
+			select * 
+				from #TempVersionResults;
+
+			drop table #TempVersionResults;
+		end 
+
+	end
+
+	select imps.BuildVersion, vers.SQLVersionDescription, imps.Description, imps.URL
+		from #SQLColumnstoreImprovements imps
+			inner join #SQLBranches branch
+				on imps.SQLBranch = branch.SQLBranch
+			inner join #SQLVersions vers
+				on imps.BuildVersion = vers.SQLVersion
+		where BuildVersion > @SQLServerBuild 
+			and branch.SQLBranch = ServerProperty('ProductLevel')
+			and branch.MinVersion < BuildVersion;
+
+	drop table #SQLColumnstoreImprovements;
+	drop table #SQLBranches;
+	drop table #SQLVersions;
+
+	--------------------------------------------------------------------------------------------------------------------
+	-- Trace Flags part
+	create table #ActiveTraceFlags(	
+		TraceFlag nvarchar(20) not null,
+		Status bit not null,
+		Global bit not null,
+		Session bit not null );
+
+	insert into #ActiveTraceFlags
+		exec sp_executesql N'DBCC TRACESTATUS()';
+
+	create table #ColumnstoreTraceFlags(
+		TraceFlag int not null,
+		Description nvarchar(500) not null,
+		URL nvarchar(600),
+		SupportedStatus bit not null 
+	);
+
+	insert into #ColumnstoreTraceFlags (TraceFlag, Description, URL, SupportedStatus )
+		values 
+		(  634, 'Disables the background columnstore compression task.', 'https://msdn.microsoft.com/en-us/library/ms188396.aspx', 1 ),
+		(  834, 'Enable Large Pages', 'https://support.microsoft.com/en-us/kb/920093?wa=wsignin1.0', 0 ),
+		(  646, 'Gets text output messages that show what segments (row groups) were eliminated during query processing', 'http://social.technet.microsoft.com/wiki/contents/articles/5611.verifying-columnstore-segment-elimination.aspx', 1 ),
+		( 9453, 'Disables Batch Execution Mode', 'http://www.nikoport.com/2016/07/24/clustered-columnstore-indexes-part-35-trace-flags-query-optimiser-rules/', 1 );
+		
+	select tf.TraceFlag, isnull(conf.Description,'Unrecognized') as Description, isnull(conf.URL,'-') as URL, SupportedStatus
+		from #ActiveTraceFlags tf
+			left join #ColumnstoreTraceFlags conf
+				on conf.TraceFlag = tf.TraceFlag
+		where @showUnrecognizedTraceFlags = 1 or (@showUnrecognizedTraceFlags = 0 AND Description is not null);
+
+	drop table #ColumnstoreTraceFlags;
+	drop table #ActiveTraceFlags;
+
+end
