da1b19c99afbfdb2e436113078c71337b5cc8956
Niko Neugebauer <info@webcaravela.com>
2017-08-16 01:46:45 +0100

    1.5.0 GetDictionaries SQL Server 2014
    Missing Files Commit

--- SQL-2014/StoredProcs/cstore_GetDictionaries.sql
+++ SQL-2014/StoredProcs/cstore_GetDictionaries.sql
@@ -1,9 +1,9 @@
 /*
 	Columnstore Indexes Scripts Library for SQL Server 2014: 
 	Dictionaries Analysis - Shows detailed information about the Columnstore Dictionaries
-	Version: 1.4.2, December 2016
+	Version: 1.5.0, August 2017
 
-	Copyright 2015-2016 Niko Neugebauer, OH22 IS (http://www.nikoport.com/columnstore/), (http://www.oh22.is/)
+	Copyright 2015-2017 Niko Neugebauer, OH22 IS (http://www.nikoport.com/columnstore/), (http://www.oh22.is/)
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
@@ -48,6 +48,11 @@ Changes in 1.3.0
 
 Changes in 1.3.1
 	- Added support for Databases with collations different to TempDB
+
+Changes in 1.5.0
+	+ Added new parameter that allows to filter the results by specific partition number (@partitionNumber)
+	+ Added new parameter for the searching precise name of the object (@preciseSearch)
+	+ Expanded search of the schema to include the pattern search with @preciseSearch = 0
 */
 
 --------------------------------------------------------------------------------------------------------------------
@@ -77,7 +82,7 @@ GO
 /*
 	Columnstore Indexes Scripts Library for SQL Server 2014: 
 	Dictionaries Analysis - Shows detailed information about the Columnstore Dictionaries
-	Version: 1.4.2, December 2016
+	Version: 1.5.0, August 2017
 */
 alter procedure dbo.cstore_GetDictionaries(
 -- Params --
@@ -90,6 +95,8 @@ alter procedure dbo.cstore_GetDictionaries(
 	@objectId int = NULL,								-- Allows to idenitfy a table thorugh the ObjectId
 	@schemaName nvarchar(256) = NULL,					-- Allows to show data filtered down to the specified schema
 	@tableName nvarchar(256) = NULL,					-- Allows to show data filtered down to 1 particular table
+	@preciseSearch bit = 0,								-- Defines if the schema and data search with the parameters @schemaName & @tableName will be precise or pattern-like
+	@partitionNumber int = 0,							-- Allows to filter data on a specific partion.
 	@columnName nvarchar(256) = NULL,					-- Allows to filter out data base on 1 particular column name
 	@indexLocation varchar(15) = NULL,					-- Allows to filter Columnstore Indexes based on their location: Disk-Based & In-Memory
 	@indexType char(2) = NULL							-- Allows to filter Columnstore Indexes by their type, with possible values (CC for 'Clustered', NC for 'Nonclustered' or NULL for both)
@@ -99,35 +106,47 @@ alter procedure dbo.cstore_GetDictionaries(
 begin
 	set nocount on;
 
+	declare @table_object_id int = NULL;
+
+	if (@tableName is not NULL )
+		set @table_object_id = isnull(object_id(@tableName),-1);
+	else 
+		set @table_object_id = NULL;
+
 	SELECT QuoteName(object_schema_name(i.object_id)) + '.' + QuoteName(object_name(i.object_id)) as 'TableName', 
-			case i.type when 5 then 'Clustered' when 6 then 'Nonclustered' end as 'Type',
-			'Disk-Based' as [Location],	
-			p.partition_number as 'Partition',
-			(select count(rg.row_group_id) from sys.column_store_row_groups rg
-				where rg.object_id = i.object_id and rg.partition_number = p.partition_number
-					  and rg.state = 3 ) as 'RowGroups',
-			count(csd.column_id) as 'Dictionaries', 
-			sum(csd.entry_count) as 'EntriesCount',
-			min(p.rows) as 'Rows Serving',
-			cast( SUM(csd.on_disk_size)/(1024.0*1024.0) as Decimal(8,3)) as 'Total Size in MB',
-			cast( MAX(case dictionary_id when 0 then csd.on_disk_size else 0 end)/(1024.0*1024.0) as Decimal(8,3)) as 'Max Global Size in MB',
-			cast( MAX(case dictionary_id when 0 then 0 else csd.on_disk_size end)/(1024.0*1024.0) as Decimal(8,3)) as 'Max Local Size in MB'
-		FROM sys.indexes AS i
-			inner join sys.partitions AS p
-				on i.object_id = p.object_id 
-			inner join sys.column_store_dictionaries AS csd
-				on csd.hobt_id = p.hobt_id and csd.partition_id = p.partition_id
-		where i.type in (5,6)
-			and i.object_id = isnull(@objectId, i.object_id)
-			and (@tableName is null or object_name (i.object_id) like '%' + @tableName + '%')
-			and (@schemaName is null or object_schema_name(i.object_id) = @schemaName)
-			and i.data_space_id = isnull( case @indexLocation when 'In-Memory' then 0 when 'Disk-Based' then 1 else i.data_space_id end, i.data_space_id )
-			and case @indexType when 'CC' then 5 when 'NC' then 6 else i.type end = i.type
-		group by object_schema_name(i.object_id) + '.' + object_name(i.object_id), i.object_id, i.type, p.partition_number
+		case i.type when 5 then 'Clustered' when 6 then 'Nonclustered' end as 'Type',
+		case i.data_space_id when 0 then 'In-Memory' else 'Disk-Based' end as [Location],	
+		p.partition_number as 'Partition',
+		(select count(rg.row_group_id) from sys.column_store_row_groups rg
+			where rg.object_id = i.object_id and rg.partition_number = p.partition_number
+				  and rg.state = 3 ) as 'RowGroups',
+		count(csd.column_id) as 'Dictionaries', 
+		sum(csd.entry_count) as 'EntriesCount',
+		(select sum(isnull(rg.total_rows,0) - isnull(rg.deleted_rows,0)) from sys.column_store_row_groups rg
+			where rg.object_id = i.object_id and rg.partition_number = p.partition_number
+				  and rg.state = 3 ) as 'Rows Serving',
+		cast( SUM(csd.on_disk_size)/(1024.0*1024.0) as Decimal(8,3)) as 'Total Size in MB',
+		cast( MAX(case dictionary_id when 0 then csd.on_disk_size else 0 end)/(1024.0*1024.0) as Decimal(8,3)) as 'Max Global Size in MB',
+		cast( MAX(case dictionary_id when 0 then 0 else csd.on_disk_size end)/(1024.0*1024.0) as Decimal(8,3)) as 'Max Local Size in MB'
+    FROM sys.indexes AS i
+		inner join sys.partitions AS p
+			on i.object_id = p.object_id 
+		inner join sys.column_store_dictionaries AS csd
+			on csd.hobt_id = p.hobt_id and csd.partition_id = p.partition_id
+    where i.type in (5,6)
+		AND (@preciseSearch = 0 AND (@tableName is null or object_name (i.object_id) like '%' + @tableName + '%') 
+			OR @preciseSearch = 1 AND (@tableName is null or object_name (i.object_id) = @tableName) )
+		AND (@preciseSearch = 0 AND (@schemaName is null or object_schema_name( i.object_id ) like '%' + @schemaName + '%')
+			OR @preciseSearch = 1 AND (@schemaName is null or object_schema_name( i.object_id ) = @schemaName))
+		AND (ISNULL(@objectId,i.object_id) = i.object_id)
+		AND partition_number = case @partitionNumber when 0 then partition_number else @partitionNumber end
+		and i.data_space_id = isnull( case @indexLocation when 'In-Memory' then 0 when 'Disk-Based' then 1 else i.data_space_id end, i.data_space_id )
+		and case @indexType when 'CC' then 5 when 'NC' then 6 else i.type end = i.type
+	group by object_schema_name(i.object_id) + '.' + object_name(i.object_id), i.object_id, i.data_space_id, i.type, p.partition_number
 	union all
 	SELECT QuoteName(object_schema_name(i.object_id,db_id('tempdb'))) + '.' + QuoteName(object_name(i.object_id,db_id('tempdb'))) as 'TableName', 
 			case i.type when 5 then 'Clustered' when 6 then 'Nonclustered' end as 'Type',
-			'Disk-Based' as [Location],	
+			case i.data_space_id when 0 then 'In-Memory' else 'Disk-Based' end as [Location],	
 			p.partition_number as 'Partition',
 			(select count(rg.row_group_id) from tempdb.sys.column_store_row_groups rg
 				where rg.object_id = i.object_id and rg.partition_number = p.partition_number
@@ -144,29 +163,34 @@ begin
 			inner join tempdb.sys.column_store_dictionaries AS csd
 				on csd.hobt_id = p.hobt_id and csd.partition_id = p.partition_id
 		where i.type in (5,6)
-			and (@tableName is null or object_name (i.object_id,db_id('tempdb')) like '%' + @tableName + '%')
-			and (@schemaName is null or object_schema_name(i.object_id,db_id('tempdb')) = @schemaName)
+			AND (@preciseSearch = 0 AND (@tableName is null or object_name (p.object_id,db_id('tempdb')) like '%' + @tableName + '%') 
+				OR @preciseSearch = 1 AND (@tableName is null or object_name (p.object_id,db_id('tempdb')) = @tableName) )
+			AND (@preciseSearch = 0 AND (@schemaName is null or object_schema_name( p.object_id,db_id('tempdb') ) like '%' + @schemaName + '%')
+				OR @preciseSearch = 1 AND (@schemaName is null or object_schema_name( p.object_id,db_id('tempdb') ) = @schemaName))
+			AND (ISNULL(@objectId,p.object_id) = p.object_id)
+			AND partition_number = case @partitionNumber when 0 then partition_number else @partitionNumber end
 			and i.data_space_id = isnull( case @indexLocation when 'In-Memory' then 0 when 'Disk-Based' then 1 else i.data_space_id end, i.data_space_id )
 			and case @indexType when 'CC' then 5 when 'NC' then 6 else i.type end = i.type
-		group by object_schema_name(i.object_id,db_id('tempdb')) + '.' + object_name(i.object_id,db_id('tempdb')), i.object_id, i.type, p.partition_number;
+		group by object_schema_name(i.object_id,db_id('tempdb')) + '.' + object_name(i.object_id,db_id('tempdb')), i.object_id, i.type, i.data_space_id, p.partition_number;
 
 
 	if @showDetails = 1
-	SELECT QuoteName(object_schema_name(part.object_id)) + '.' + QuoteName(object_name(part.object_id)) as 'TableName',
+	select QuoteName(object_schema_name(part.object_id)) + '.' + QuoteName(object_name(part.object_id)) as 'TableName',
 			ind.name COLLATE DATABASE_DEFAULT as 'IndexName', 
 			part.partition_number as 'Partition',
 			cols.name COLLATE DATABASE_DEFAULT as ColumnName, 
 			dict.column_id as ColumnId,
-			dict.dictionary_id as 'SegmentId',
+			dict.dictionary_id as 'DictionaryId',
 			tp.name COLLATE DATABASE_DEFAULT as ColumnType,
-			dict.column_id as 'ColumnId', 
 			case dictionary_id when 0 then 'Global' else 'Local' end as 'Type', 
-			part.rows as 'Rows Serving', 
+			(select sum(isnull(rg.total_rows,0) - isnull(rg.deleted_rows,0)) from sys.column_store_row_groups rg
+				where rg.object_id = part.object_id and rg.partition_number = part.partition_number
+					  and rg.state = 3 ) as 'Rows Serving', 
 			entry_count as 'Entry Count', 
 			cast( on_disk_size / 1024. / 1024. as Decimal(8,2)) 'SizeInMb'
 		from sys.column_store_dictionaries dict
 			inner join sys.partitions part
-				ON dict.hobt_id = part.hobt_id and dict.partition_id = part.partition_id
+				ON dict.partition_id = part.partition_id and dict.partition_id = part.partition_id
 			inner join sys.indexes ind
 				on part.object_id = ind.object_id and part.index_id = ind.index_id
 			inner join sys.columns cols
@@ -191,9 +215,12 @@ begin
 					when 'sysname' then 1
 				end = 1
 			) OR @showAllTextDictionaries = 0 )
-			and ind.object_id = isnull(@objectId, ind.object_id)
-			and (@tableName is null or object_name (ind.object_id) like '%' + @tableName + '%')
-			and (@schemaName is null or object_schema_name(ind.object_id) = @schemaName)
+			AND (@preciseSearch = 0 AND (@tableName is null or object_name (part.object_id) like '%' + @tableName + '%') 
+				OR @preciseSearch = 1 AND (@tableName is null or object_name (part.object_id) = @tableName) )
+			and (@preciseSearch = 0 AND (@schemaName is null or object_schema_name( part.object_id ) like '%' + @schemaName + '%')
+				OR @preciseSearch = 1 AND (@schemaName is null or object_schema_name( part.object_id ) = @schemaName))
+			AND (ISNULL(@objectId,part.object_id) = part.object_id)
+			AND partition_number = case @partitionNumber when 0 then partition_number else @partitionNumber end
 			and cols.name = isnull(@columnName,cols.name)
 			and case dictionary_id when 0 then 'Global' else 'Local' end = isnull(@showDictionaryType, case dictionary_id when 0 then 'Global' else 'Local' end)
 			and ind.data_space_id = isnull( case @indexLocation when 'In-Memory' then 0 when 'Disk-Based' then 1 else ind.data_space_id end, ind.data_space_id )
@@ -204,11 +231,12 @@ begin
 			part.partition_number as 'Partition',
 			cols.name as ColumnName, 
 			dict.column_id as ColumnId,
-			dict.dictionary_id as 'SegmentId',
+			dict.dictionary_id as 'DictionaryId',
 			tp.name as ColumnType,
-			dict.column_id as 'ColumnId', 
 			case dictionary_id when 0 then 'Global' else 'Local' end as 'Type', 
-			part.rows as 'Rows Serving', 
+			(select sum(isnull(rg.total_rows,0) - isnull(rg.deleted_rows,0)) from sys.column_store_row_groups rg
+				where rg.object_id = part.object_id and rg.partition_number = part.partition_number
+					  and rg.state = 3 ) as 'Rows Serving',
 			entry_count as 'Entry Count', 
 			cast( on_disk_size / 1024. / 1024. as Decimal(8,2)) 'SizeInMb'
 		from tempdb.sys.column_store_dictionaries dict
@@ -238,9 +266,12 @@ begin
 					when 'sysname' then 1
 				end = 1
 			) OR @showAllTextDictionaries = 0 )
-			and (@tableName is null or object_name(ind.object_id,db_id('tempdb')) like '%' + @tableName + '%')
-			and (@schemaName is null or object_schema_name(ind.object_id,db_id('tempdb')) = @schemaName)
-			and cols.name = isnull(@columnName,cols.name)
+			AND (@preciseSearch = 0 AND (@tableName is null or object_name (part.object_id,db_id('tempdb')) like '%' + @tableName + '%') 
+				OR @preciseSearch = 1 AND (@tableName is null or object_name (part.object_id,db_id('tempdb')) = @tableName) )
+			AND (@preciseSearch = 0 AND (@schemaName is null or object_schema_name( part.object_id,db_id('tempdb') ) like '%' + @schemaName + '%')
+				OR @preciseSearch = 1 AND (@schemaName is null or object_schema_name( part.object_id,db_id('tempdb') ) = @schemaName))
+			AND (ISNULL(@objectId,part.object_id) = part.object_id)
+			AND partition_number = case @partitionNumber when 0 then partition_number else @partitionNumber end			and cols.name = isnull(@columnName,cols.name)
 			and case dictionary_id when 0 then 'Global' else 'Local' end = isnull(@showDictionaryType, case dictionary_id when 0 then 'Global' else 'Local' end)
 			and ind.data_space_id = isnull( case @indexLocation when 'In-Memory' then 0 when 'Disk-Based' then 1 else ind.data_space_id end, ind.data_space_id )
 			and case @indexType when 'CC' then 5 when 'NC' then 6 else ind.type end = ind.type
